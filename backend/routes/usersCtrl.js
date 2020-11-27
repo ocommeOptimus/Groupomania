a778ffee0b764afae5a16fd0b879ef3f96da3f3f
JeremyBoisdur <jeremy.boisdur@digitregroup.com>
2020-11-27 01:08:34 +0100

    Added regex to email and password

--- backend/routes/usersCtrl.js
+++ backend/routes/usersCtrl.js
@@ -2,6 +2,8 @@ const bcrypt = require('bcrypt')
 const jwtUtils = require('../utils/jwt.utils')
 const models = require('../models')
 
+const EMAIL_REGEX = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+const PASSWORD_REGEX = /^(?=.*\d).{4,8}$/
 
 //FIRST WAY
 module.exports = {
@@ -13,6 +15,18 @@ module.exports = {
 
         if (email == null || username == null || password == null) {
             return res.status(400).json({ 'error': 'missing parameters' })
+        }
+
+        if (username.length >= 13 || username.length <= 4) {
+            return res.status(400).json({ 'error': 'wrong username (must be length 5 - 12)' })
+        }
+
+        if (!EMAIL_REGEX.test(email)) {
+            return res.status(400).json({ 'error': 'email is not valid' })
+        }
+
+        if (!PASSWORD_REGEX.test(password)) {
+            return res.status(400).json({ 'error': 'password invalid (must length 4 - 8 and include 1 number at least' })
         }
 
         models.User.findOne({
