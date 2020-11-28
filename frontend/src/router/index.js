108ff239ebdca6a87f5f9f1cd8b6bef4dedb11e4
JeremyBoisdur <jeremy.boisdur@digitregroup.com>
2020-11-28 17:15:08 +0100

    Set register & login pages

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -13,10 +13,17 @@ const routes = [
   {
     path: '/about',
     name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/register',
+    name: 'Register',
+    component: () => import(/* webpackChunkName: "about" */ '../views/Register.vue')
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: () => import('../views/Login.vue')
   }
 ]
 
