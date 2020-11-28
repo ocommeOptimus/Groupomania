f86e57517d4f26fc6339d960703b71792d5ce33c
JeremyBoisdur <jeremy.boisdur@digitregroup.com>
2020-11-28 17:15:08 +0100

    Set register & login pages

---
+++ frontend/src/components/Navbar.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <router-link class="navbar-brand" to="/">Groupomania</router-link>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item active">
+            <router-link class="nav-link" to="/">Home <span class="sr-only">(current)</span></router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/register">Register</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/login">Login</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/about">About</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link disabled" to="#" tabindex="-1" aria-disabled="true">Logout</router-link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+  props: {
+    
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
---
+++ frontend/src/views/Login.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="login my-3 mx-md-5">
+    <form>
+      <div class="form-group p-3">
+        <input type="email" class="form-control" id="exampleInputEmail1" placeholder="your@email.com" v-model="email">
+      </div>
+      <div class="form-group p-3">
+        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" v-model="password">
+      </div>
+      <button type="submit" class="btn btn-primary mx-3" @click="userLogin">Log me in !</button>
+    </form>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'Login',
+  data() {
+    return {
+      email: '',
+      password: ''
+    }
+  }, methods: {
+    userLogin() {
+      let userLogin = {
+        email: this.email,
+        password: this.password
+      }
+      axios.post('http://localhost:3000/auth/login', userLogin)
+    }
+  }
+}
+</script>
