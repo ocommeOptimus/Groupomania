12322d562ac40fd32cda1173eef825eba9e90bfa
JeremyBoisdur <jeremy.boisdur@digitregroup.com>
2020-11-28 17:15:08 +0100

    Set register & login pages

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,18 +1,11 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="home my-3 text-center">
+    <img alt="Vue logo" class="w-25" src="../assets/logo.png">
+    <h1>Bienvenue !</h1>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
 </script>
---
+++ frontend/src/views/Register.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="register my-3 mx-md-5">
+    <form>
+      <div class="form-group px-3">
+        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="your@email.com" aria-describedby="emailRules" v-model="email">
+        <small id="emailRules" class="form-text text-muted">We'll never share your email with anyone else.</small>
+      </div>
+      <div class="form-group px-3">
+        <input type="text" class="form-control" id="exampleFormControlInput2" placeholder="Username" aria-describedby="usernameRules" v-model="username">
+        <small id="usernameRules" class="form-text text-muted">Must be at least 4 characters and less than 12.</small>
+      </div>
+      <div class="form-group px-3">
+        <textarea class="form-control" id="exampleFormControlTextarea1" placeholder="Your bio" rows="3" aria-describedby="bioRules" v-model="bio"></textarea>
+        <small id="usernameRules" class="form-text text-muted">Not required but way more pleasant to have one.</small>
+      </div>
+      <div class="form-group px-3">
+        <input type="password" class="form-control" placeholder="Password" id="exampleInputPassword1" v-model="password">
+        <small id="passwordRules" class="form-text text-muted">Must be at least 5 characters.</small>
+      </div>
+      <button type="submit" class="btn btn-primary mx-3" @click="userRegister">Register</button>
+    </form>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'Register',
+  data() {
+    return {
+      email: '',
+      username: '',
+      bio: '',
+      password: ''
+    }
+  }, methods: {
+    userRegister() {
+      let newUser = {
+        email: this.email,
+        username: this.username,
+        bio: this.bio,
+        password: this.password
+      }
+      axios.post('http://localhost:3000/auth/register', newUser)
+    }
+  }
+}
+</script>
